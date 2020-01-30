573680e96242d3edb79955114bf2620e36aead3b
Courtney DeRiggs <courtney.deriggs@gmail.com>
2020-01-30 16:53:46 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+# Add  code here
+
+require 'benchmark'
+require 'pry'
+
+def prime?(integer)
+    interger = [-1,0,1,2...]
+      # binding.pry
+     integer.each { |integer| integer % 2 == 0?  "prime" : "composite" }
+end
+
+
+  # i.each { |integer,number| n%i!=0 && i<=n/2 == false puts "prime"}
