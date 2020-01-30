1d081f8241329a8b61b4020af84bb1a2d317de9a
Courtney DeRiggs <courtney.deriggs@gmail.com>
2020-01-30 18:14:41 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,13 +1,28 @@
 # Add  code here
-
+# 
 require 'benchmark'
 require 'pry'
 
-def prime?(integer)
-    interger = [-1,0,1,2...]
-      # binding.pry
-     integer.each { |integer| integer % 2 == 0?  "prime" : "composite" }
+def prime(number)
+  if number < 2
+      false
+  else
+      (2..number -1). to_a.all? do |n|
+      number % n! = 0
+    end
+  end
 end
 
 
-  # i.each { |integer,number| n%i!=0 && i<=n/2 == false puts "prime"}
+
+
+
+
+# def prime?(integer)
+#     interger = [-1,0,1,2...]
+#       # binding.pry
+#      integer.each { |integer| integer % 2 == 0?  "prime" : "composite" }
+# end
+# 
+# 
+#   # i.each { |integer,number| n%i!=0 && i<=n/2 == false puts "prime"}
